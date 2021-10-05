e7d73ddf394d2a36ebde853480b6c1f4a28c0ff1
AnhTu1207 <dinhanhtu1207@gmail.com>
2021-10-05 18:55:39 +0700

    Add errors when trying to delete category

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+const Sequelize = require('sequelize')
 
 const { CategoryService } = require(appRoot + "/services");
 const { map } = require("lodash");
@@ -91,6 +92,9 @@ class CategoryController {
             }
         }
         catch (e) {
+            if (e instanceof Sequelize.ForeignKeyConstraintError) {
+                return res.status(400).json({ status: 400, message: e.parent.detail });
+            }
             if (e.errors && e.errors.length) {
                 return res
                     .status(400)
