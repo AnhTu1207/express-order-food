d9aa8e4ec4bc61a7fc83116fe147d66a3ed5ada5
AnhTu1207 <dinhanhtu1207@gmail.com>
2021-11-30 02:44:17 +0700

    Add custom auth for food

--- src/routes/FoodRoute.js
+++ src/routes/FoodRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const { AddFoodRequest, UpdateFoodRequest } = require(appRoot + "/requests");
+const { FoodAuth } = require(appRoot + "/middlewares");
 const router = express.Router();
 
 const { FoodController } = require(appRoot + "/controllers");
@@ -10,10 +11,10 @@ router.get("/show/:id", FoodController.show);
 router.get("/showByCategory/:id", FoodController.showByCategory)
 router.get("/showByStore/:id", FoodController.showByStore)
 router.get("/find/", FoodController.search)
-router.post("/", AddFoodRequest, FoodController.store);
-router.post("/uploadImage/:id", FoodController.upload);
-router.post("/editImage/:id", FoodController.edit)
-router.put("/:id", UpdateFoodRequest, FoodController.update)
-router.delete("/:id", FoodController.delete)
+router.post("/", AddFoodRequest, FoodAuth, FoodController.store);
+router.post("/uploadImage/:id", FoodAuth, FoodController.upload);
+router.post("/editImage/:id", FoodAuth, FoodController.edit)
+router.put("/:id", [FoodAuth, UpdateFoodRequest], FoodController.update)
+router.delete("/:id", FoodAuth, FoodController.delete)
 
 module.exports = router;
--- src/routes/OptionLabelRoute.js
+++ src/routes/OptionLabelRoute.js
@@ -1,14 +1,15 @@
 const express = require("express");
 
 const { AddOptionLabelRequest, UpdateOptionLabelRequest } = require(appRoot + "/requests");
+const { FoodAuth } = require(appRoot + "/middlewares");
 const router = express.Router();
 
 const { OptionLabelController } = require(appRoot + "/controllers");
 
 router.get("/", OptionLabelController.index);
 router.get("/show/:id", OptionLabelController.show);
-router.post("/", AddOptionLabelRequest, OptionLabelController.store);
-router.put("/:id", UpdateOptionLabelRequest, OptionLabelController.update)
-router.delete("/:id", OptionLabelController.delete);
+router.post("/", [FoodAuth, AddOptionLabelRequest], OptionLabelController.store);
+router.put("/:id", [FoodAuth, UpdateOptionLabelRequest], OptionLabelController.update)
+router.delete("/:id", FoodAuth, OptionLabelController.delete);
 
 module.exports = router;
