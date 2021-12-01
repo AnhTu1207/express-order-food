02ae6f3cd115053bf5442683c9a9aac06a611f8d
AnhTu1207 <dinhanhtu1207@gmail.com>
2021-12-01 23:52:22 +0700

    Remove escape for category

--- src/requests/CategoryRequest/UpdateCategoryRequest.js
+++ src/requests/CategoryRequest/UpdateCategoryRequest.js
@@ -1,7 +1,7 @@
 const { check, body } = require("express-validator");
 
 module.exports = [
-    check("name").not().isEmpty().trim().escape(),
+    check("name").not().isEmpty().trim()
     body().custom(body => {
         const keys = ['name'];
         return Object.keys(body).every(key => keys.includes(key));
