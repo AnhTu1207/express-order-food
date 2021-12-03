c0b9e23f4b6148ca927ba6dcd42065ee18aa680e
AnhTu1207 <dinhanhtu1207@gmail.com>
2021-12-03 13:33:44 +0700

    Update request and model

--- src/requests/StoreRequest/AddStoreRequest.js
+++ src/requests/StoreRequest/AddStoreRequest.js
@@ -3,8 +3,8 @@ const { check, body } = require("express-validator");
 module.exports = [
     check("name").not().isEmpty().trim().escape(),
     check("address").not().isEmpty().trim(),
-    check("latitude").not().isEmpty().trim().escape().matches(/^(\+|-)?(?:90(?:(?:\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,6})?))$/).withMessage("Latitude is incorrect format"),
-    check("longitude").not().isEmpty().trim().escape().matches(/^(\+|-)?(?:180(?:(?:\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,6})?))$/).withMessage("Longitude is incorrect format"),
+    check("latitude").not().isEmpty().trim().escape().matches(/^(\+|-)?(?:90(?:(?:\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,7})?))$/).withMessage("Latitude is incorrect format"),
+    check("longitude").not().isEmpty().trim().escape().matches(/^(\+|-)?(?:180(?:(?:\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,7})?))$/).withMessage("Longitude is incorrect format"),
     check("email").not().isEmpty().trim().escape(),
     check("password").not().isEmpty().trim().escape(),
     check("phone").not().isEmpty().trim().escape().matches(/(84|0[3|5|7|8|9])+([0-9]{8})\b/g).withMessage("Your phone number is not in the correct format"),
